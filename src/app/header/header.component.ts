5c9cebe43d1e84a711942eb578b3c4c42cc8ba14
Cristian Ramírez <cristianrd92@gmail.com>
2020-10-14 20:47:36 -0300

    Se implementa header de manera correcta ademas se hacen cambios varios

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,13 @@ import { AuthService } from '../usuarios/auth.service';
 import swal from "sweetalert2";
 import { Router } from '@angular/router';
 import { faCity } from '@fortawesome/free-solid-svg-icons';
+import { faBuilding } from '@fortawesome/free-solid-svg-icons';
+import { faUniversity } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 @Component({
   selector: 'app-header',
@@ -10,7 +17,13 @@ import { faCity } from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent {
   title: string = 'Syllabus Web';
+  // Se deben declarar todos los iconos que se usaran en el header
   faCity = faCity;
+  faBuilding= faBuilding;
+  faUniversity= faUniversity;
+  faCalendarAlt= faCalendarAlt;
+  faHome= faHome;
+  
   constructor(public authService:AuthService, private router: Router){}
   logout():void{
     swal("Logout","Has cerrado sesión con éxito","success");
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../usuarios/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService:AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
